1d2856e1591cab321e50497a021925819513ea9a
AMAN KAUSHIK <amankaushik@AMANs-MacBook-Air.local>
2026-07-02 21:01:21 +0530

    using two pointer

---
+++ Array/Check_if_array_is_sorted.py
@@ -0,0 +1,13 @@
+"""Given an array nums, return true if the array was originally sorted in non-decreasing order
+"""
+
+class is_sorted():
+    def check(self, nums: list[int]) -> bool:
+        for i in range(0,len(nums)-1):
+            if nums[i]>nums[i+1]:
+                return False
+        return True
+    
+
+# Time Complexity: O(n)
+# space complexity: O(1)
